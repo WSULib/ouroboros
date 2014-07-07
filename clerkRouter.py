fa2c28467c800eddd579d322212baf3a3c76c3b9
root <root@digital.library.wayne.edu>
2014-07-07 16:19:07 -0400

    RDF regex modifications

--- clerkRouter.py
+++ clerkRouter.py
@@ -1,38 +1,39 @@
-import xmltodict, json
+import json
 from lib.FOXML2Solr.FOXML2Solr import FOXML2Solr
 from lib.WSUAPI.WSUAPImain import WSUAPImain
 from lib.imageServer.imageServerMain import imageWork
 from lib.Projects.ProjectsMain import ProjectsMain
+from lib.fedoraConsumer import fedoraConsumer
 
 class clerkRouter:
 	
-	# handles events in Fedora Commons as reported by JSM
-	def fedConsumer(self,**kwargs):			
-		msg = kwargs['msg']		
+	# # handles events in Fedora Commons as reported by JSM
+	# def fedConsumer(self,**kwargs):			
+	# 	msg = kwargs['msg']		
 
-		# create dictionary from XML string
-		try:
-			msgDict = xmltodict.parse(msg)
-			# pull info
-			fedEvent = msgDict['entry']['title']['#text']			
+	# 	# create dictionary from XML string
+	# 	try:
+	# 		msgDict = xmltodict.parse(msg)
+	# 		# pull info
+	# 		fedEvent = msgDict['entry']['title']['#text']			
 
-			# testing, print results			
-			print "Action:",fedEvent
+	# 		# testing, print results			
+	# 		print "Action:",fedEvent
 
-			# modify, purge
-			if fedEvent.startswith("modify") or fedEvent.startswith("purge"):
-				PID = msgDict['entry']['category'][0]['@term']		
-				print "Object PID:", PID
-				FOXML2Solr.delay(fedEvent,PID)
-			# ingest
-			if fedEvent.startswith('ingest'):
-				PID = msgDict['entry']['content']['#text']
-				print "Object PID:", PID
-				FOXML2Solr.delay(fedEvent,PID)
+	# 		# modify, purge
+	# 		if fedEvent.startswith("modify") or fedEvent.startswith("purge"):
+	# 			PID = msgDict['entry']['category'][0]['@term']		
+	# 			print "Object PID:", PID
+	# 			FOXML2Solr.delay(fedEvent,PID)
+	# 		# ingest
+	# 		if fedEvent.startswith('ingest'):
+	# 			PID = msgDict['entry']['content']['#text']
+	# 			print "Object PID:", PID
+	# 			FOXML2Solr.delay(fedEvent,PID)
 
-		except Exception,e:
-			print "Actions based on fedEvent failed or were not performed."
-			print str(e)		
+	# 	except Exception,e:
+	# 		print "Actions based on fedEvent failed or were not performed."
+	# 		print str(e)		
 
 	# handles WSUAPI requests
 	def WSUAPI(self,**kwargs):		

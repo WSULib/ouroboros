0cce8f867b9fc01d0cfcc15237ecc30bbc7d2ee3
root <root@digital.library.wayne.edu>
2014-07-14 14:10:38 -0400

    added objectState module

---
+++ fedoraManager2/actions/objectState/__init__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import requests
+import json
+import sys
+import ast
+import os
+import xml.etree.ElementTree as ET
+import urllib, urllib2
+import datetime
+from lxml import etree
+from flask import Blueprint, render_template, redirect, abort
+
+from fedoraManager2.fedoraHandles import fedora_handle
+
+
+objectState = Blueprint('objectState', __name__, template_folder='templates', static_folder="static")
+
+
+@objectState.route('/objectState')
+def index():	
+	return render_template("objectState.html")
+
+
+
+def objectState_worker(job_package):
+	form_data = job_package['form_data']
+	print form_data
+
+	# in confirmation present, change state
+	if form_data['confirm_string'] == "CONFIRM":
+
+		# grab target state
+		target_state = form_data['target_state']
+
+		# set state	
+		print "Setting state to: {target_state}".format(target_state=target_state)
+		
+		# get PID handle, set state, save()
+		PID = job_package['PID']		
+		obj_ohandle = fedora_handle.get_object(PID)		
+		obj_ohandle.state = target_state
+		return obj_ohandle.save()
+
+	else:
+		return "Confirmation not entered correctly, skipping."
+
+

3373718f026689460e52841186e7f95fcb188961
root <root@digital.library.wayne.edu>
2014-07-15 19:45:46 -0400

    submissions taking place

--- fedoraManager2/actions/editDSXML/__init__.py
+++ fedoraManager2/actions/editDSXML/__init__.py
@@ -19,15 +19,41 @@ import re
 # eulfedora
 import eulfedora
 
-
-
-
 # create blueprint
-editDSXML = Blueprint('editDSXML', __name__, template_folder='templates')
+editDSXML = Blueprint('editDSXML', __name__, template_folder='templates', static_folder="static", static_url_path='/static/editDSXML')
 
+'''
+UI notes:
+	- need to iterate through PIDs for each editor...
+'''
 
 # main view
 @editDSXML.route('/editDSXML', methods=['POST', 'GET'])
 def index():
 	
 	return render_template("editDSXML.html")
+
+
+# update handler
+@editDSXML.route('/editDSXML/update', methods=['POST', 'GET'])
+def update():	
+	'''
+	New raw XML contained in request.data
+	'''
+
+	# get object info
+	PID = "wayne:Fake02a" # need to actually get PID...		
+	DS = "MODS" # need to get datastream...
+
+	# initialized DS object
+	obj_ohandle = fedora_handle.get_object(PID)
+	newDS = eulfedora.models.DatastreamObject(obj_ohandle, DS, DS, control_group="X")	
+
+	# construct DS object	
+	newDS.mimetype = "text/xml"
+	# content		
+	newDS.content = request.data
+	# save constructed object
+	print newDS.save()
+
+	return "Updated."	

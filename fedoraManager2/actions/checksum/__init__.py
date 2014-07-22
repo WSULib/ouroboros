80e936d14c8240602bb2e3aa3c0617b08f06c723
root <root@digital.library.wayne.edu>
2014-07-22 16:26:15 -0400

    initial checksum work

---
+++ fedoraManager2/actions/checksum/__init__.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+import requests
+import json
+import sys
+import ast
+import os
+import xml.etree.ElementTree as ET
+import urllib, urllib2
+import datetime
+from lxml import etree
+from flask import Blueprint, render_template, redirect, abort
+
+from fedoraManager2.fedoraHandles import fedora_handle
+
+
+checksum = Blueprint('checksum', __name__, template_folder='templates', static_folder="static")
+
+
+@checksum.route('/checksum')
+def index():	
+	return render_template("checksum.html")
+
+
+
+def checksum_worker(job_package):
+	form_data = job_package['form_data']
+	print form_data
+
+	# in confirmation present, change state
+	if form_data['confirm_string'] == "CONFIRM":
+
+		# grab target state
+		target_state = form_data['target_state']
+
+		# set state	
+		print "Setting state to: {target_state}".format(target_state=target_state)
+		
+		# get PID handle, set state, save()
+		PID = job_package['PID']		
+		obj_ohandle = fedora_handle.get_object(PID)		
+		obj_ohandle = obj_ohandle.ds_list
+		for (name, loc) in obj_ohandle.items():
+			print name
+
+		# getDatastreamObject('ACCESS').checksum_type
+		# getDatastreamObject('ACCESS').checksum
+		# take name, insert into .checksum and checksum_type
+		# return datastream name and checksum results to page (which are then sorted by template)
+		# gotta figure out what return does in this context
+		return obj_ohandle
+
+		# Enable Checksumming feature to be developed if Checksums are not enabled
+
+	else:
+		return "Confirmation not entered correctly, skipping."
+
+

40a012ee294d6ca31d0235fcd65286826aac93c9
root <root@digital.library.wayne.edu>
2014-07-28 14:59:35 -0400

    celery broker and backend configurations moved to localConfig.py

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,9 +1,11 @@
+from localConfig import *
+
 # RabbitMQ
 # BROKER_URL = 'amqp://guest:guest@mitten:5672//'
 
 # Redis
-BROKER_URL='redis://localhost:6379/0',
-RESULT_BACKEND='redis://localhost:6379/1',
-RESULT_SERIALIZER='json',
+BROKER_URL='redis://localhost:6379/{redis_broker_db}'.format(redis_broker_db=str(redis_broker_db))
+RESULT_BACKEND='redis://localhost:6379/{redis_backend_db}'.format(redis_backend_db=str(redis_backend_db))
+RESULT_SERIALIZER='json'
 
 

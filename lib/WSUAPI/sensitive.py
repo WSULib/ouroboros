520cf250ba02a52000c1ad7358b1ce1b5bdb8b99
Cole Hudson <cole.hudson@wayne.edu>
2014-08-04 10:08:00 -0400

    merging in purge_datastream branch

---
+++ lib/WSUAPI/sensitive.py
@@ -0,0 +1,6 @@
+#sensitive info for FOXML2Solr
+username = "fedoraAdmin"
+password = "cowp00p2012"
+
+# salt for account username/password hahses
+userAccountSalt = "Wayne State University Digital Object Repository"

12c5542651a9b9e6ef34798d9cf51c2b7e0ff13d
Graham Hukill <ghukill@gmail.com>
2014-08-05 10:59:08 -0400

    RELS editing improving

--- fedoraManager2/static/actions/editRELS.js
+++ fedoraManager2/static/actions/editRELS.js
@@ -2,12 +2,44 @@
 
 
 // modify relationship
-function modifyRel(){
-	
+function modifyRel(self){
+	console.log("About to modify...");
+	var new_predicate = self.parent().siblings('td.predicate').children('input').val();
+	var old_predicate = self.parent().siblings('td.predicate').children('input').attr('name');
+	var new_object = self.parent().siblings('td.object').children('input').val();
+	var old_object = self.parent().siblings('td.object').children('input').attr('name');
+
+	// send pred / obj to modify_relationship
+    $.ajax({
+		url: "/fireTask/editRELS_modify_worker",
+		type: "POST",
+		data: {
+			"new_predicate":new_predicate,
+			"old_predicate":old_predicate,
+			"new_object":new_object,
+			"old_object":old_object
+		}
+	}).done(function(response) { 
+		window.location.href="/userJobs";
+	});
 }
 
 
 // purge relationship
-function removeRel(){
+function removeRel(predicate,object){
+	console.log("About to purge...");
+	console.log(predicate,object);
+
+	// send pred / obj to purge_relationship
+    $.ajax({
+		url: "/fireTask/editRELS_purge_worker",
+		type: "POST",
+		data: {
+			"predicate":predicate,
+			"object":object
+		}
+	}).done(function(response) { 
+		window.location.href="/userJobs";
+	});
 
 }

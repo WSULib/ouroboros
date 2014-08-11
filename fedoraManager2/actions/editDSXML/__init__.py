f026e49fa92052d3490cba72b5192d9df2c3f4f3
Graham Hukill <ghukill@gmail.com>
2014-08-11 14:24:39 -0400

    updates for domain variable

--- fedoraManager2/actions/editDSXML/__init__.py
+++ fedoraManager2/actions/editDSXML/__init__.py
@@ -43,7 +43,7 @@ def index(pid_num):
 	session['editDSXML_PID'] = PID
 	session['editDSXML_DS'] = DS
 
-	return render_template("editDSXML.html",PIDs=PIDs,PID=PID,pid_num=int(pid_num),APP_HOST=localConfig.APP_HOST)
+	return render_template("editDSXML.html", PIDs=PIDs, PID=PID, pid_num=int(pid_num), APP_HOST=localConfig.APP_HOST, DOMAIN=localConfig.DOMAIN)
 
 # update handler
 @editDSXML.route('/editDSXML/update', methods=['POST', 'GET'])

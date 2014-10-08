172331e00ad16b0c65ac8aaa66b3096fe4ca5eb4
Graham Hukill <ghukill@gmail.com>
2014-10-08 16:29:36 -0400

    fixed editsRELS, no longer accepting ajax calls to POST, now needs form submissions

--- fedoraManager2/static/actions/editRELS.js
+++ fedoraManager2/static/actions/editRELS.js
@@ -1,27 +1,25 @@
 // js file for editRELS action
 
-
 // modify relationship
 function modifyRel(self){
 	console.log("About to modify...");
 	var new_predicate = self.parent().siblings('td.predicate').children('input').val();
 	var old_predicate = self.parent().siblings('td.predicate').children('input').attr('name');
 	var new_object = self.parent().siblings('td.object').children('input').val();
-	var old_object = self.parent().siblings('td.object').children('input').attr('name');
+	var old_object = self.parent().siblings('td.object').children('input').attr('name');	
 
-	// send pred / obj to modify_relationship
-    $.ajax({
-		url: "/fireTask/editRELS_modify_worker",
-		type: "POST",
-		data: {
-			"new_predicate":new_predicate,
-			"old_predicate":old_predicate,
-			"new_object":new_object,
-			"old_object":old_object
-		}
-	}).done(function(response) { 
-		window.location.href="/userJobs";
-	});
+	// create and submit form (results in redirect)
+	var url = "/fireTask/editRELS_modify_worker";
+
+	var form = $('<form action="' + url + '" method="POST">' +
+	'<input type="hidden" name="new_predicate" value="' + new_predicate + '" />' +
+	'<input type="hidden" name="old_predicate" value="' + old_predicate + '" />' +
+	'<input type="hidden" name="new_object" value="' + new_object + '" />' +
+	'<input type="hidden" name="old_object" value="' + old_object + '" />' +
+	'</form>');
+	
+	// submit
+	form.submit();	
 }
 
 
@@ -30,16 +28,15 @@ function removeRel(predicate,object){
 	console.log("About to purge...");
 	console.log(predicate,object);
 
-	// send pred / obj to purge_relationship
-    $.ajax({
-		url: "/fireTask/editRELS_purge_worker",
-		type: "POST",
-		data: {
-			"predicate":predicate,
-			"object":object
-		}
-	}).done(function(response) { 
-		window.location.href="/userJobs";
-	});
+	// create and submit form (results in redirect)
+	var url = "/fireTask/editRELS_purge_worker";
+
+	var form = $('<form action="' + url + '" method="POST">' +
+	'<input type="hidden" name="predicate" value="' + predicate + '" />' +
+	'<input type="hidden" name="object" value="' + object + '" />' +	
+	'</form>');
+	
+	// submit
+	form.submit();
 
 }

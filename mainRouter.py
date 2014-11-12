c122676880d8bffd82e6a9d53c79b7c57b9716af
Graham Hukill <ghukill@gmail.com>
2014-11-11 22:14:20 -0500

    fall cleaning - removed Projects from /lib

--- mainRouter.py
+++ mainRouter.py
@@ -1,7 +1,6 @@
 import json
 
 from lib.imageServer.imageServerMain import imageWork
-from lib.Projects.ProjectsMain import ProjectsMain
 
 
 class mainRouter:	
@@ -20,28 +19,5 @@ class mainRouter:
 			print "imageServer call unsuccessful.  Error:",str(e)
 			return '{{"imageServerstatus":{exceptionErrorString}}}'.format(exceptionErrorString=json.dumps(str(e)))
 
-	# handles events in Fedora Commons as reported by JSM
-	def Projects(self,**kwargs):
-		response = {}		
-		getParams = kwargs['getParams']
-		requestPath = kwargs['requestPath']
-
-		# # run WSUAPImain(), return results to fedClerk
-		try:			
-			response = ProjectsMain(getParams,requestPath)					
-			return response
-		except Exception,e:
-			print "Projects call unsuccessful.  Error:",str(e)
-			response['headers'] = {}
-			response['headers']['Access-Control-Allow-Origin'] = '*'
-			response['headers']['Access-Control-Allow-Methods'] = 'GET, POST'
-			response['headers']['Access-Control-Allow-Headers'] = 'x-prototype-version,x-requested-with'
-			response['headers']['Access-Control-Max-Age'] = 2520
-			response['headers']["content-type"] = "application/json"
-			response['headers']['Connection'] = 'Close'
-			response['headers']['X-Powered-By'] = 'ShoppingHorse'
-			response['content'] = '{{"WSUAPIstatus":{exceptionErrorString}}}'.format(exceptionErrorString=json.dumps(str(e)))
-			return response
-	
 
 

b91b502a32769ccd3eac42922d1a2604b5cca68c
Graham Hukill <ghukill@gmail.com>
2015-01-13 12:21:52 -0500

    broad strokes of Audio ContentType

--- WSUDOR_ContentTypes/WSUDOR_Audio.py
+++ WSUDOR_ContentTypes/WSUDOR_Audio.py
@@ -25,6 +25,9 @@ from WSUDOR_Manager.solrHandles import solr_handle
 from WSUDOR_Manager.fedoraHandles import fedora_handle
 from WSUDOR_Manager import redisHandles, helpers
 
+# audio conversions and libraries
+from pydub import AudioSegment
+
 
 class WSUDOR_Audio(WSUDOR_ContentTypes.WSUDOR_GenObject):
 
@@ -116,7 +119,6 @@ class WSUDOR_Audio(WSUDOR_ContentTypes.WSUDOR_GenObject):
 			# write objMeta as datastream
 			objMeta_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "OBJMETA", "Ingest Bag Object Metadata", mimetype="application/json", control_group='M')
 			objMeta_handle.label = "Ingest Bag Object Metadata"
-			# objMeta_handle.content = json.dumps(self.objMeta)
 			file_path = self.Bag.path + "/data/objMeta.json"
 			objMeta_handle.content = open(file_path)
 			objMeta_handle.save()
@@ -130,6 +132,12 @@ class WSUDOR_Audio(WSUDOR_ContentTypes.WSUDOR_GenObject):
 			
 			# isRepresentedBy
 			self.ohandle.add_relationship("http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isRepresentedBy",self.objMeta['isRepresentedBy'])
+
+			# determine isRepresentedBy filename
+			for DS in self.objMeta['datastreams']:
+				if DS['ds_id'] == self.objMeta['isRepresentedBy']:
+					# isRepresentedBy_filename = DS['filename']
+					isRepresentedBy_filepath = self.Bag.path + "/data/datastreams/" + DS['filename']
 			
 			# hasContentModel
 			content_type_string = str("info:fedora/CM:"+self.objMeta['content_type'].split("_")[1])
@@ -143,78 +151,107 @@ class WSUDOR_Audio(WSUDOR_ContentTypes.WSUDOR_GenObject):
 			MODS_handle.content = open(file_path)
 			MODS_handle.save()
 
+			# prime PLAYLIST list of datastreams
+			playlist_list = []
+
 			# create derivatives and write datastreams
+			count = 1			
 			for ds in self.objMeta['datastreams']:
+
 				file_path = self.Bag.path + "/data/datastreams/" + ds['filename']
 				print "Looking for:",file_path
 
-				# original
+				# if no 'order' key, or not integer (e.g. None), assign incrementing number
+				if 'order' not in ds.keys() or not isinstance( ds['order'], int ):
+					print "Could not find valid 'order' key for datastream dictionary, assigning",len(self.objMeta['datastreams']) + count
+					ds['order'] = len(self.objMeta['datastreams']) + count
+					# bump ds counter
+					count += 1
+
+				# original, untouched
 				orig_handle = eulfedora.models.FileDatastreamObject(self.ohandle, ds['ds_id'], ds['label'], mimetype=ds['mimetype'], control_group='M')
 				orig_handle.label = ds['label']
 				orig_handle.content = open(file_path)
 				orig_handle.save()
+
+				# make mp3 derivative
+				'''
+				!! Consider always doing, even if MP3, to ensure consistency in file format? !!
+				'''
+				temp_filename = "/tmp/Ouroboros/"+str(uuid.uuid4())+".mp3"
+				audio_file_handle = AudioSegment.from_file(file_path)
+				mp3_file_handle = audio_file_handle.export(temp_filename,format="mp3")
+				mp3_file_handle.close()
+				if os.path.getsize(temp_filename) < 1:
+					print "Converted file is empty, could not create derivative MP3."
+				else:
+					print "Derivative mp3 created."
+				mp3_ds_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "{ds_id}_MP3".format(ds_id=ds['ds_id']), "{label}_MP3".format(label=ds['label']), mimetype="audio/mpeg", control_group='M')
+				mp3_ds_handle.label = ds['label']
+				mp3_ds_handle.content = open(temp_filename)
+				mp3_ds_handle.save()
+				os.system('rm {temp_filename}'.format(temp_filename=temp_filename))		
+
+
+				# create thumbnail and preview waveforms for datastreams
+				'''
+				Using BBC's audiowaveform:
+				https://github.com/bbcrd/audiowaveform
+
+				e.g. audiowaveform -i rainforest.wav -o /var/www/wsuls/dev/graham/dropbox/test2.png -w 640 --waveform-color 0c5449ff --background-color 00000000 --no-axis-labels
+				'''
+				temp_filename = "/tmp/Ouroboros/"+str(uuid.uuid4())+".png"
+				os.system("audiowaveform -i {input_file} -o {temp_filename} -w 1280 --waveform-color 0c5449ff --background-color FFFFFFFF --no-axis-labels".format(input_file=isRepresentedBy_filepath, temp_filename=temp_filename))
 				
-				# # make thumb			
-				# temp_filename = "/tmp/Ouroboros/"+str(uuid.uuid4())+".jpg"
-				# im = Image.open(file_path)
-				# width, height = im.size
-				# max_width = 200	
-				# max_height = 200
-				# im.thumbnail((max_width, max_height), Image.ANTIALIAS)
-				# if im.mode != "RGB":
-				# 	im = im.convert("RGB")
-				# im.save(temp_filename,'JPEG')
-				# thumb_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "{ds_id}_THUMBNAIL".format(ds_id=ds['ds_id']), "{label}_THUMBNAIL".format(label=ds['label']), mimetype="image/jpeg", control_group='M')
-				# thumb_handle.label = "{label}_THUMBNAIL".format(label=ds['label'])
-				# thumb_handle.content = open(temp_filename)
-				# thumb_handle.save()
-				# os.system('rm {temp_filename}'.format(temp_filename=temp_filename))
+				# preview (do first, downsizing from here)				
+				im = Image.open(temp_filename)	
+				width, height = im.size
+				max_width = 1280	
+				max_height = 960		
+				im.save(temp_filename,"JPEG")
+				rep_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "{ds_id}_PREVIEW".format(ds_id=ds['ds_id']), "{label}_PREVIEW".format(label=ds['label']), mimetype="image/jpeg", control_group="M")
+				rep_handle.content = open(temp_filename)
+				rep_handle.label = "{label}_PREVIEW".format(label=ds['label'])
+				rep_handle.save()
 
-				# # make preview
-				# temp_filename = "/tmp/Ouroboros/"+str(uuid.uuid4())+".jpg"
-				# im = Image.open(file_path)
-				# width, height = im.size
-				# max_width = 1280	
-				# max_height = 960
-				# im.thumbnail((max_width, max_height), Image.ANTIALIAS)
-				# if im.mode != "RGB":
-				# 	im = im.convert("RGB")
-				# im.save(temp_filename,'JPEG')
-				# preview_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "{ds_id}_PREVIEW".format(ds_id=ds['ds_id']), "{label}_PREVIEW".format(label=ds['label']), mimetype="image/jpeg", control_group='M')
-				# preview_handle.label = "{label}_PREVIEW".format(label=ds['label'])
-				# preview_handle.content = open(temp_filename)
-				# preview_handle.save()
-				# os.system('rm {temp_filename}'.format(temp_filename=temp_filename))
+				# thumbnail				
+				im = Image.open(temp_filename)	
+				width, height = im.size
+				max_width = 200	
+				max_height = 200
+				im.thumbnail((max_width, max_height), Image.ANTIALIAS)		
+				im.save(temp_filename,"JPEG")
+				rep_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "{ds_id}_THUMBNAIL".format(ds_id=ds['ds_id']), "{label}_THUMBNAIL".format(label=ds['label']), mimetype="image/jpeg", control_group="M")
+				rep_handle.content = open(temp_filename)
+				rep_handle.label = "{label}_THUMBNAIL".format(label=ds['label'])
+				rep_handle.save()				
+				
+				# remove temp waveform
+				os.system('rm {temp_filename}'.format(temp_filename=temp_filename))
 
-				# # make jp2
-				# temp_filename = "/tmp/Ouroboros/"+str(uuid.uuid4())+".jp2"
-				# os.system("convert {input} {output}[256x256]".format(input=file_path,output=temp_filename))
-				# jp2_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "{ds_id}_JP2".format(ds_id=ds['ds_id']), "{label}_JP2".format(label=ds['label']), mimetype="image/jp2", control_group='M')
-				# jp2_handle.label = "{label}_JP2".format(label=ds['label'])
-				# try:
-				# 	jp2_handle.content = open(temp_filename)
-				# except:
-				# 	# sometimes jp2 creation results in two files, look for first one in this instance
-				# 	temp_filename = temp_filename.split(".")[0]
-				# 	temp_filename = temp_filename + "-0.jp2"
-				# 	jp2_handle.content = open(temp_filename)
-				# jp2_handle.save()
-				# os.system('rm {temp_filename}'.format(temp_filename=temp_filename))
+				# append to playlist_list
+				playlist_list.append(ds)
 
-				# # add to RELS-INT
-				# fedora_handle.api.addRelationship(self.ohandle,'info:fedora/{pid}/{ds_id}'.format(pid=self.ohandle.pid,ds_id=ds['ds_id']),'info:fedora/fedora-system:def/relations-internal#isPartOf','info:fedora/{pid}'.format(pid=self.ohandle.pid))
-				# fedora_handle.api.addRelationship(self.ohandle,'info:fedora/{pid}/{ds_id}_THUMBNAIL'.format(pid=self.ohandle.pid,ds_id=ds['ds_id']),'info:fedora/fedora-system:def/relations-internal#isThumbnailOf','info:fedora/{pid}/{ds_id}'.format(pid=self.ohandle.pid,ds_id=ds['ds_id']))
-				# fedora_handle.api.addRelationship(self.ohandle,'info:fedora/{pid}/{ds_id}_JP2'.format(pid=self.ohandle.pid,ds_id=ds['ds_id']),'info:fedora/fedora-system:def/relations-internal#isJP2Of','info:fedora/{pid}/{ds_id}'.format(pid=self.ohandle.pid,ds_id=ds['ds_id']))
-				# fedora_handle.api.addRelationship(self.ohandle,'info:fedora/{pid}/{ds_id}_PREVIEW'.format(pid=self.ohandle.pid,ds_id=ds['ds_id']),'info:fedora/fedora-system:def/relations-internal#isPreviewOf','info:fedora/{pid}/{ds_id}'.format(pid=self.ohandle.pid,ds_id=ds['ds_id']))
+			# write PLAYLIST datastream with playlist_list
+			print "Generating PLAYLIST datastream"
+			for ds in playlist_list:
+				ds['thumbnail'] = "http://digital.library.wayne.edu/fedora/objects/{pid}/datastreams/{ds_id}_THUMBNAIL/content".format(pid=self.ohandle.pid,ds_id=ds['ds_id'])
+				ds['preview'] = "http://digital.library.wayne.edu/fedora/objects/{pid}/datastreams/{ds_id}_PREVIEW/content".format(pid=self.ohandle.pid,ds_id=ds['ds_id'])
+				ds['mp3'] = "http://digital.library.wayne.edu/fedora/objects/{pid}/datastreams/{ds_id}_MP3/content".format(pid=self.ohandle.pid,ds_id=ds['ds_id'])
+
+			playlist_handle = eulfedora.models.DatastreamObject(self.ohandle,"PLAYLIST", "PLAYLIST", mimetype="application/json", control_group="M")
+			playlist_handle.content = json.dumps( sorted(playlist_list, key=lambda k: k['order']) )
+			playlist_handle.label = "PLAYLIST"
+			playlist_handle.save()
 
 
-			# write generic thumbnail and preview
+			# write generic thumbnail and preview for object
 			for gen_type in ['THUMBNAIL','PREVIEW']:
-				thumb_rep_handle = eulfedora.models.DatastreamObject(self.ohandle,gen_type, gen_type, mimetype="image/jpeg", control_group="R")
-				thumb_rep_handle.ds_location = "http://digital.library.wayne.edu/fedora/objects/{pid}/datastreams/{ds_id}_{gen_type}/content".format(pid=self.ohandle.pid,ds_id=self.objMeta['isRepresentedBy'],gen_type=gen_type)
-				thumb_rep_handle.label = gen_type
-				thumb_rep_handle.save()
-
+				rep_handle = eulfedora.models.DatastreamObject(self.ohandle,gen_type, gen_type, mimetype="image/jpeg", control_group="R")
+				rep_handle.ds_location = "http://digital.library.wayne.edu/fedora/objects/{pid}/datastreams/{ds_id}_{gen_type}/content".format(pid=self.ohandle.pid,ds_id=self.objMeta['isRepresentedBy'],gen_type=gen_type)
+				rep_handle.label = gen_type
+				rep_handle.save()
+	
 
 			# save and commit object before finishIngest()
 			final_save = self.ohandle.save()
--- WSUDOR_ContentTypes/__init__.py
+++ WSUDOR_ContentTypes/__init__.py
@@ -1,4 +1,5 @@
 from WSUDOR_Object import WSUDOR_Object, WSUDOR_GenObject
 from WSUDOR_Image import WSUDOR_Image
 from WSUDOR_Collection import WSUDOR_Collection
+from WSUDOR_Audio import WSUDOR_Audio
 from WSUDOR_WSUebook import WSUDOR_WSUebook 

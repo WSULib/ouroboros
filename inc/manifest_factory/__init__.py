18ee2b8cf45056a1921d3c35f3f6bedd3458a0af
Graham Hukill <ghukill@gmail.com>
2015-12-22 13:25:42 -0500

    quick fix to iiif factory config

--- inc/manifest_factory/__init__.py
+++ inc/manifest_factory/__init__.py
@@ -6,11 +6,10 @@ iiif_manifest_factory_instance = factory.ManifestFactory()
 # Where the resources live on the web
 iiif_manifest_factory_instance.set_base_metadata_uri("http:/digital.library.wayne.edu/iiif_manifest")
 # Where the resources live on disk
-iiif_manifest_factory_instance.set_base_metadata_dir("/tmp/iiif_manifest")
+iiif_manifest_factory_instance.set_base_metadata_dir("/tmp")
 
 # Default Image API information
 iiif_manifest_factory_instance.set_base_image_uri("http://digital.library.wayne.edu/loris")
-# iiif_manifest_factory_instance.set_base_image_uri("http://digital.library.wayne.edu/loris_dev") #loris_dev
 iiif_manifest_factory_instance.set_iiif_image_info(2.0, 2) # Version, ComplianceLevel
 
 # 'warn' will print warnings, default level

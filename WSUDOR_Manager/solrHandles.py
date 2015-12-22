8abb1b9e128ea9f1aac03bba6fc26f608c011a7f
Graham Hukill <ghukill@gmail.com>
2015-12-22 13:54:52 -0500

    graceful fail on solrhandles

--- WSUDOR_Manager/solrHandles.py
+++ WSUDOR_Manager/solrHandles.py
@@ -6,11 +6,14 @@ import localConfig
 session = requests.Session()
 
 # Core used for search and retrieval (e.g. powers front-end API)
-solr_handle = Solr('http://silo.lib.wayne.edu/solr4/{SOLR_SEARCH_CORE}'.format(SOLR_SEARCH_CORE=localConfig.SOLR_SEARCH_CORE, make_request=session))
+try:
+	solr_handle = Solr('http://silo.lib.wayne.edu/solr4/{SOLR_SEARCH_CORE}'.format(SOLR_SEARCH_CORE=localConfig.SOLR_SEARCH_CORE, make_request=session))
 
-# Core used for management, indexing, deletion
-solr_manage_handle = Solr('http://silo.lib.wayne.edu/solr4/{SOLR_MANAGE_CORE}'.format(SOLR_MANAGE_CORE=localConfig.SOLR_MANAGE_CORE, make_request=session))
+	# Core used for management, indexing, deletion
+	solr_manage_handle = Solr('http://silo.lib.wayne.edu/solr4/{SOLR_MANAGE_CORE}'.format(SOLR_MANAGE_CORE=localConfig.SOLR_MANAGE_CORE, make_request=session))
 
-# Core used for bookreader fulltext
-solr_bookreader_handle = Solr('http://silo.lib.wayne.edu/solr4/{SOLR_BOOKREADER_CORE}'.format(SOLR_BOOKREADER_CORE=localConfig.SOLR_BOOKREADER_CORE, make_request=session))
+	# Core used for bookreader fulltext
+	solr_bookreader_handle = Solr('http://silo.lib.wayne.edu/solr4/{SOLR_BOOKREADER_CORE}'.format(SOLR_BOOKREADER_CORE=localConfig.SOLR_BOOKREADER_CORE, make_request=session))
+except:
+	print "Could not setup solr handles"
 

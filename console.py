3f503b23377e59ad8c6fa5c4b32c1667f66983a6
Graham Hukill <ghukill@gmail.com>
2016-01-04 13:41:11 -0500

    tolerance to console from solr

--- console.py
+++ console.py
@@ -1,5 +1,8 @@
 from WSUDOR_Manager import *
 fedora_handle = fedoraHandles.fedora_handle
-solr_handle = solrHandles.solr_handle
-solr_manage_handle = solrHandles.solr_manage_handle
-solr_bookreader_handle = solrHandles.solr_bookreader_handle
+try:
+	solr_handle = solrHandles.solr_handle
+	solr_manage_handle = solrHandles.solr_manage_handle
+	solr_bookreader_handle = solrHandles.solr_bookreader_handle
+except:
+	print "Could not load solr handles, skipping."

550d0175dd4c0c8071f3054abb540cdf41babe5e
Graham Hukill <ghukill@gmail.com>
2016-01-05 11:00:31 -0500

    cleanup

--- console.py
+++ console.py
@@ -1,8 +1,5 @@
 from WSUDOR_Manager import *
 fedora_handle = fedoraHandles.fedora_handle
-try:
-	solr_handle = solrHandles.solr_handle
-	solr_manage_handle = solrHandles.solr_manage_handle
-	solr_bookreader_handle = solrHandles.solr_bookreader_handle
-except:
-	print "Could not load solr handles, skipping."
+solr_handle = solrHandles.solr_handle
+solr_manage_handle = solrHandles.solr_manage_handle
+solr_bookreader_handle = solrHandles.solr_bookreader_handle

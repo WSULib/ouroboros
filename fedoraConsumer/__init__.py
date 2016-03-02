67d1913c5a1ae7f6977846143b028c1c24758f7a
Graham Hukill <ghukill@gmail.com>
2016-03-02 11:54:08 -0500

    related work to user rework

--- fedoraConsumer/__init__.py
+++ fedoraConsumer/__init__.py
@@ -33,10 +33,8 @@ def fedoraConsumer(self,**kwargs):
 		if fedEvent.startswith("purge"):
 			PID = msgDict['entry']['category'][0]['@term']		
 			print "Object PID:", PID
-
-			# index to Solr if SOLR_AUTOINDEX is True
-			if SOLR_AUTOINDEX == True:
-				pruneSolr_worker.delay(None,PID=PID)
+			
+			pruneSolr_worker.delay(None,PID=PID)
 
 		# ingest
 		if fedEvent.startswith('ingest'):

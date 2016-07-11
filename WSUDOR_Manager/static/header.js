878e43ecbef663f01bfd68015321eba034a5c6d0
Cole Hudson <cole.hudson@wayne.edu>
2016-07-11 13:44:56 +0000

    js listener and action for using enter key with page searcher box

--- WSUDOR_Manager/static/header.js
+++ WSUDOR_Manager/static/header.js
@@ -7,10 +7,12 @@ $(function() {
   } 
  
   var availablePages = JSON.parse(localStorage.getItem("pageSearch"));
+  var selected_item = "";
 
   $(".autocomplete").autocomplete({
     source: availablePages
   }).data("ui-autocomplete")._renderItem = function (ul, item) {
+    selected_item = item;
     return $("<li></li>")
         .data("item.autocomplete", item)
         .append('<div><button><a class="ac-item-a" href="//' + item.url + '" target="_blank">' + item.label + '</a></button></div>')
@@ -20,15 +22,7 @@ $(function() {
 
 $(".page_searcher").keypress(function(e) {
   if(e.which == 13) {
-    console.log("yeppers");
-  $(".autocomplete").autocomplete({
-        source: availablePages
-      }).data("ui-autocomplete")._renderItem = function (ul, item) {
-    return $("<li></li>")
-        .data("item.autocomplete", item)
-        .append('<div><button><a class="ac-item-a" href="//' + item.url + '" target="_blank">' + item.label + '</a></button></div>')
-        .appendTo(ul);
-      }
+    window.location = '//' + selected_item.url;
   } //enter key
 }) //keypress
 

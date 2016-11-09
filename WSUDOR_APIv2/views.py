f971ed0344bb7c64da3ba3a781abd21d44686284
Graham Hukill <ghukill@gmail.com>
2016-11-09 10:54:15 -0500

    initial notes and docs

---
+++ WSUDOR_APIv2/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# WSUDOR_APIv2
+
+# Ouroboros
+import localConfig
+from WSUDOR_Manager.helpers import gzipped
+
+# python modules
+import json
+
+# flask proper
+from flask import render_template, request, session, redirect, make_response, Response
+

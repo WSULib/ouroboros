12da6b5207c6b79d4a59076f0d4cd139d63bb2bc
Graham Hukill <ghukill@gmail.com>
2016-11-09 22:03:03 +0000

    rough testing of flask-RESTful looking promising

--- WSUDOR_API/models.py
+++ WSUDOR_API/models.py
@@ -6,12 +6,35 @@
 import localConfig
 
 # modules
-from flask_restful import Resource
+from flask_restful import reqparse, Resource
 
 # WSUDOR_API_app
 from WSUDOR_API import api
 
-
+#################################################################################
+# TESTING
+#################################################################################
 class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+
+    def get(self, name):
+        return {'hello': name}
+
+
+class ArgParsing(Resource):
+
+
+	def get(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument('goober', type=int, help='the particular integrity of goober')
+		parser.add_argument('tronic', type=str, help='pecularities of tronic')
+		args = parser.parse_args(strict=True)
+
+		'''
+		If it 'goober' fails the type=int restriction above, it aborts here and returns a 400 with message = help from above
+		if include strict=True in parser.parse_args, squawks if anything but 'goober' or 'tronic' in GET/POST params
+		'''
+
+		return {
+			'goober_integrity': args['goober'],
+			'pecularities_of_tronic': args['tronic']
+		}

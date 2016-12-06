de7042a92af4dded74be2a798c4c6adfeb10f8b7
Graham Hukill <ghukill@gmail.com>
2016-12-06 18:29:43 +0000

    cleanup before merge into dual-api

--- WSUDOR_API/views.py
+++ WSUDOR_API/views.py
@@ -10,18 +10,12 @@ from WSUDOR_API import WSUDOR_API_app, api, models
 
 
 # IDENTIFY
-#################################################################################
 api.add_resource(models.Identify, '/%(API_PREFIX)s' % {'API_PREFIX':localConfig.WSUDOR_API_PREFIX}, endpoint='identify')
 
-
 # ITEMS
-#################################################################################
 api.add_resource(models.Item, '/%(API_PREFIX)s/item/<string:pid>' % {'API_PREFIX':localConfig.WSUDOR_API_PREFIX}, endpoint='item')
 
-
-
 # SEARCH
-#################################################################################
 api.add_resource(models.Search, '/%(API_PREFIX)s/search' % {'API_PREFIX':localConfig.WSUDOR_API_PREFIX}, endpoint='search')
 api.add_resource(models.CollectionSearch, '/%(API_PREFIX)s/collection/<string:pid>/search' % {'API_PREFIX':localConfig.WSUDOR_API_PREFIX}, endpoint='collection_search')
 
@@ -29,8 +23,9 @@ api.add_resource(models.CollectionSearch, '/%(API_PREFIX)s/collection/<string:pi
 
 
 
+
+
 # TESTING
-#################################################################################
 api.add_resource(models.HelloWorld, '/%(API_PREFIX)s/hello/<string:name>' % {'API_PREFIX':localConfig.WSUDOR_API_PREFIX}, endpoint='helloworld')
 api.add_resource(models.ArgParsing, '/%(API_PREFIX)s/goober' % {'API_PREFIX':localConfig.WSUDOR_API_PREFIX}, endpoint='goober_integrity')
 

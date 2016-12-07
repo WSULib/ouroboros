9ec8498ad1a72c62494ace4797ce8032813a3b27
Graham Hukill <ghukill@gmail.com>
2016-12-07 20:59:06 +0000

    versions working - next to fix slash routing changes

--- WSUDOR_API/v1/__init__.py
+++ WSUDOR_API/v1/__init__.py
@@ -3,14 +3,12 @@ import localConfig
 API_VERSION = 1
 def gen_api_prefix(API_VERSION=API_VERSION):
 	return "/%(APP_PREFIX)s/v%(API_VERSION)d" % {'APP_PREFIX':localConfig.WSUDOR_API_PREFIX.lstrip('/'), 'API_VERSION':API_VERSION}
-print "setting api version prefix: %s" % gen_api_prefix()
 
 # import umbrella API app
 from WSUDOR_API import WSUDOR_API_app
 
 # import local views and handlers
-import bitStream, lorisProxy
-import views
+import views, bitStream, lorisProxy
 
 # register blueprints
 WSUDOR_API_app.register_blueprint(views.views_blueprint, url_prefix=gen_api_prefix())

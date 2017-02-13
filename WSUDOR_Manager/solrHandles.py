05e20704ac18ebf606d53f3e142232c41dea1ab3
Cole Hudson <cole.hudson@wayne.edu>
2017-02-13 14:11:07 -0500

    Revert "removing sessions from solrhandle"
    This reverts commit 3aba55fdd8e726541f380677ed74767ce1f775d6.

--- WSUDOR_Manager/solrHandles.py
+++ WSUDOR_Manager/solrHandles.py
@@ -2,14 +2,17 @@ from mysolr import Solr
 import requests
 import localConfig
 
+# set connection through requests
+session = requests.Session()
+
 # Core used for search and retrieval (e.g. powers front-end API)
 try:
 	# single, primary search core
-	solr_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_SEARCH_CORE))
+	solr_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_SEARCH_CORE), make_request=session)
 	# Core used for bookreader fulltext
-	solr_bookreader_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_BOOKREADER_CORE))
+	solr_bookreader_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_BOOKREADER_CORE), make_request=session)
 	# Core used for WSUDOR user accounts
-	solr_user_handle = Solr('http://localhost/solr4/users')
+	solr_user_handle = Solr('http://localhost/solr4/users', make_request=session)
 except:
 	print "Could not setup solr handles"
 	solr_handle = False
@@ -18,6 +21,6 @@ except:
 
 def onDemand(core):
 	try:
-		return Solr('http://localhost/solr4/%s' % (core))
+		return Solr('http://localhost/solr4/%s' % (core), make_request=session)
 	except:
 		return False

86df05f53e7c39a44ac9efbf38010b550d00a597
Cole Hudson <cole.hudson@wayne.edu>
2017-02-13 20:55:59 +0000

    set solr version

--- WSUDOR_Manager/solrHandles.py
+++ WSUDOR_Manager/solrHandles.py
@@ -3,24 +3,16 @@ import requests
 import localConfig
 
 # set connection through requests
-session = requests.Session()
+# session = requests.Session()
 
 # Core used for search and retrieval (e.g. powers front-end API)
-try:
-	# single, primary search core
-	solr_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_SEARCH_CORE), make_request=session)
-	# Core used for bookreader fulltext
-	solr_bookreader_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_BOOKREADER_CORE), make_request=session)
-	# Core used for WSUDOR user accounts
-	solr_user_handle = Solr('http://localhost/solr4/users', make_request=session)
-except:
-	print "Could not setup solr handles"
-	solr_handle = False
-	solr_bookreader_handle = False
+# single, primary search core
+solr_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_SEARCH_CORE), version=4)
+# Core used for bookreader fulltext
+solr_bookreader_handle = Solr('http://localhost/solr4/%s' % (localConfig.SOLR_BOOKREADER_CORE), version=4)
+# Core used for WSUDOR user accounts
+solr_user_handle = Solr('http://localhost/solr4/users', version=4)
 
 
 def onDemand(core):
-	try:
-		return Solr('http://localhost/solr4/%s' % (core), make_request=session)
-	except:
-		return False
+    return Solr('http://localhost/solr4/%s' % (core), version=4)

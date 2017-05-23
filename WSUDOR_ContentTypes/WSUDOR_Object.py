09ec3d6a6b1d3ced16d54017c8e111a9e20d3922
Graham Hukill <ghukill@gmail.com>
2017-05-23 15:22:52 +0000

    changing out localhosts

--- WSUDOR_ContentTypes/WSUDOR_Object.py
+++ WSUDOR_ContentTypes/WSUDOR_Object.py
@@ -356,9 +356,8 @@ class WSUDOR_GenObject(object):
         XSLhand = open('inc/xsl/RELS-EXT_extract.xsl','r')
         xslt_tree = etree.parse(XSLhand)
         transform = etree.XSLT(xslt_tree)
-        # raw, unmodified RDF
-        raw_xml_URL = "http://localhost/fedora/objects/%s/datastreams/RELS-EXT/content" % (self.pid)
-        raw_xml = requests.get(raw_xml_URL).text.encode("utf-8")
+        # raw, unmodified RDF via Eulfedora
+        raw_xml = self.ohandle.rels_ext.content.serialize()
         XMLroot = etree.fromstring(raw_xml)
         SolrXML = transform(XMLroot)
         return xmltodict.parse(str(SolrXML))
@@ -370,9 +369,8 @@ class WSUDOR_GenObject(object):
         XSLhand = open('inc/xsl/RELS-EXT_extract.xsl','r')
         xslt_tree = etree.parse(XSLhand)
         transform = etree.XSLT(xslt_tree)
-        # raw, unmodified RDF
-        raw_xml_URL = "http://localhost/fedora/objects/%s/datastreams/RELS-INT/content" % (self.pid)
-        raw_xml = requests.get(raw_xml_URL).text.encode("utf-8")
+        # raw, unmodified RDF via Eulfedora
+        raw_xml = self.ohandle.getDatastreamObject('RELS-INT').content.serialize()
         XMLroot = etree.fromstring(raw_xml)
         SolrXML = transform(XMLroot)
         return xmltodict.parse(str(SolrXML))

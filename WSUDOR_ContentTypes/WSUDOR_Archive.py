6fdb88a8fd4207d2acdbc034c799d23e3b763a94
Cole Hudson <colehudson@gmail.com>
2018-03-16 19:50:55 +0000

    lgbt detroit and archival workflow additions

---
+++ WSUDOR_ContentTypes/WSUDOR_Archive.py
@@ -0,0 +1,242 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import mimetypes
+import json
+import uuid
+from PIL import Image
+import time
+import traceback
+import sys
+import requests
+
+# library for working with LOC BagIt standard 
+import bagit
+
+# celery
+from WSUDOR_Manager import celery
+
+# eulfedora
+import eulfedora
+
+# localConfig
+import localConfig
+
+# WSUDOR
+import WSUDOR_ContentTypes
+from WSUDOR_ContentTypes import logging
+logging = logging.getChild("WSUDOR_Object")
+from WSUDOR_Manager.solrHandles import solr_handle
+from WSUDOR_Manager.fedoraHandles import fedora_handle
+from WSUDOR_Manager import redisHandles, helpers, models
+
+
+class WSUDOR_Archive(WSUDOR_ContentTypes.WSUDOR_GenObject):
+
+	# static values for class
+	label = "Archive"
+	description = "This ContentType applies to compressed collections of files or images of hard disks, iso files, tar, zip, etc.  The intent is to create a representative thumbnail for this item."
+	Fedora_ContentType = "CM:Archive"
+	version = 1
+
+	def __init__(self,object_type=False,content_type=False,payload=False,orig_payload=False):
+		
+		# run __init__ from parent class
+		WSUDOR_ContentTypes.WSUDOR_GenObject.__init__(self,object_type, content_type, payload, orig_payload)
+		
+		# Add WSUDOR_Archive struct_requirements to WSUDOR_Object instance struct_requirements
+		self.struct_requirements['WSUDOR_Archive'] = {
+			"datastreams":[],
+			"external_relationships":[]
+		}
+
+		# content-type methods run and returned to API
+		self.public_api_additions = [self.object_hierarchy]
+
+		# OAIexposed (on ingest, register OAI identifier)
+		self.OAIexposed = True
+
+
+	# perform ingestTest
+	def validIngestBag(self, indexObject=True):
+
+		def report_failure(failure_tuple):
+			if results_dict['verdict'] == True : results_dict['verdict'] = False
+			results_dict['failed_tests'].append(failure_tuple)
+
+		# reporting
+		results_dict = {
+			"verdict":True,
+			"failed_tests":[]
+		}
+
+		# check that content_type is a valid ContentType				
+		if self.__class__ not in WSUDOR_ContentTypes.WSUDOR_GenObject.__subclasses__():
+			report_failure(("Valid ContentType","WSUDOR_Object instance's ContentType: %s, not found in acceptable ContentTypes: %s " % (self.content_type, WSUDOR_ContentTypes.WSUDOR_GenObject.__subclasses__())))
+
+		# finally, return verdict
+		return results_dict
+
+
+	# ingest image type
+	def ingestBag(self, indexObject=True):
+
+		if self.object_type != "bag":
+			raise Exception("WSUDOR_Object instance is not 'bag' type, aborting.")
+
+
+		# attempt to ingest bag / object
+		try:		
+			
+			self.ohandle = fedora_handle.get_object(self.objMeta['id'],create=True)
+			self.ohandle.save()
+
+			# set base properties of object
+			self.ohandle.label = self.objMeta['label']
+
+			# write POLICY datastream
+			# NOTE: 'E' management type required, not 'R'
+			logging.debug("Using policy: %s" % self.objMeta['policy'])
+			policy_suffix = self.objMeta['policy'].split("info:fedora/")[1]
+			policy_handle = eulfedora.models.DatastreamObject(self.ohandle,"POLICY", "POLICY", mimetype="text/xml", control_group="E")
+			policy_handle.ds_location = "http://localhost/fedora/objects/%s/datastreams/POLICY_XML/content" % (policy_suffix)
+			policy_handle.label = "POLICY"
+			policy_handle.save()
+
+			# write objMeta as datastream
+			objMeta_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "OBJMETA", "Ingest Bag Object Metadata", mimetype="application/json", control_group='M')
+			objMeta_handle.label = "Ingest Bag Object Metadata"
+			file_path = self.Bag.path + "/data/objMeta.json"
+			objMeta_handle.content = open(file_path)
+			objMeta_handle.save()
+
+			# write explicit RELS-EXT relationships
+			for relationship in self.objMeta['object_relationships']:
+				logging.debug("Writing relationship: %s %s" % (str(relationship['predicate']),str(relationship['object'])))
+				self.ohandle.add_relationship(str(relationship['predicate']),str(relationship['object']))
+			
+			# writes derived RELS-EXT
+			# isRepresentedBy
+			'''
+			if present, isRepresentedBy relationship from objMeta trumps pre-existing relationships
+			'''
+			if 'isRepresentedBy' in self.objMeta.keys():
+				# purge old ones
+				for s,p,o in self.ohandle.rels_ext.content:
+					if str(p) == 'http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isRepresentedBy':
+						logging.debug('found pre-existing isRepresentedBy relationship, %s, removing as we have one from objMeta' % str(o))
+						self.ohandle.purge_relationship('http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isRepresentedBy',o)
+				logging.debug("writing isRepresentedBy from objMeta: %s" % self.objMeta['isRepresentedBy'])
+				self.ohandle.add_relationship("http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isRepresentedBy",self.objMeta['isRepresentedBy'])
+			
+			# hasContentModel
+			content_type_string = str("info:fedora/CM:"+self.objMeta['content_type'].split("_")[1])
+			logging.debug("Writing ContentType relationship: info:fedora/fedora-system:def/relations-external#hasContentModel %s" % content_type_string)
+			self.ohandle.add_relationship("info:fedora/fedora-system:def/relations-external#hasContentModel",content_type_string)
+			self.ohandle.add_relationship("http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/preferredContentModel",content_type_string)
+
+			# set discoverability if datastreams present
+			if len(self.objMeta['datastreams']) > 0:
+				self.ohandle.add_relationship("http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isDiscoverable","info:fedora/True")
+			else:
+				self.ohandle.add_relationship("http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isDiscoverable","info:fedora/False")
+
+			# write MODS datastream if MODS.xml exists
+			if os.path.exists(self.Bag.path + "/data/MODS.xml"):
+				MODS_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "MODS", "MODS descriptive metadata", mimetype="text/xml", control_group='M')
+				MODS_handle.label = "MODS descriptive metadata"
+				file_path = self.Bag.path + "/data/MODS.xml"
+				MODS_handle.content = open(file_path)
+				MODS_handle.save()
+
+			else:
+				# write generic MODS datastream
+				MODS_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "MODS", "MODS descriptive metadata", mimetype="text/xml", control_group='M')
+				MODS_handle.label = "MODS descriptive metadata"
+
+				raw_MODS = '''
+<mods:mods xmlns:mods="http://www.loc.gov/mods/v3" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" version="3.4" xsi:schemaLocation="http://www.loc.gov/mods/v3 http://www.loc.gov/standards/mods/v3/mods-3-4.xsd">
+  <mods:titleInfo>
+    <mods:title>%s</mods:title>
+  </mods:titleInfo>
+  <mods:identifier type="local">%s</mods:identifier>
+  <mods:extension>
+    <PID>%s</PID>
+  </mods:extension>
+</mods:mods>
+				''' % (self.objMeta['label'], self.objMeta['id'].split(":")[1], self.objMeta['id'])
+				logging.debug("%s" % raw_MODS)
+				MODS_handle.content = raw_MODS		
+				MODS_handle.save()
+
+
+			# write PREMIS if exists
+			if os.path.exists(self.Bag.path + "/data/PREMIS.xml"):
+				logging.debug("writing PREMIS datastream")
+				PREMIS_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "PREMIS", "PREMIS preservation metadadta", mimetype="text/xml", control_group='M')
+				PREMIS_handle.label = "PREMIS preservation metadadta"
+				premis_file_path = self.Bag.path + "/data/PREMIS.xml"
+				PREMIS_handle.content = open(premis_file_path)
+				PREMIS_handle.save()
+
+			# create derivatives and write datastreams
+			for ds in self.objMeta['datastreams']:
+
+				if ds['mimetype'] == "application/octet-stream":
+					rep_handle = eulfedora.models.FileDatastreamObject(self.ohandle, "FILE", "FILE", mimetype="ds['mimetype']", control_group="M")
+					file_path = self.Bag.path + "/data/datastreams/%s" % ds['filename']
+					rep_handle.label = "FILE"
+					rep_handle.content = open(file_path, 'rb')
+					rep_handle.save()
+
+
+			# make generic container images
+			preview_handle = eulfedora.models.DatastreamObject(self.ohandle, "PREVIEW", "PREVIEW", mimetype="image/png", control_group="M")
+			preview_handle.ds_location = "http://localhost/fedora/objects/wayne:WSUDORThumbnails/datastreams/Archive/content"
+			preview_handle.label = "PREVIEW"
+			preview_handle.save()
+
+			thumb_handle = eulfedora.models.DatastreamObject(self.ohandle, "THUMBNAIL", "THUMBNAIL", mimetype="image/png", control_group="M")
+			thumb_handle.ds_location = "http://localhost/fedora/objects/wayne:WSUDORThumbnails/datastreams/Archive/content"
+			thumb_handle.label = "THUMBNAIL"
+			thumb_handle.save()
+
+			# save and commit object before finishIngest()
+			final_save = self.ohandle.save()
+
+			# finish generic ingest
+			return self.finishIngest(gen_manifest=False, indexObject=indexObject, contentTypeMethods=[])
+
+
+
+		# exception handling
+		except Exception,e:
+			logging.debug("%s" % traceback.format_exc())
+			logging.debug("Image Ingest Error: %s" % e)
+			return False
+
+
+	# ingest image type
+	def genIIIFManifest(self, iiif_manifest_factory_instance, identifier, getParams):
+
+		pass
+
+
+	# default previewImage return
+	def previewImage(self):
+
+		'''
+		Return image/loris params for API to render
+			- pid, datastream, region, size, rotation, quality, format
+		'''
+		return (self.pid, 'PREVIEW', 'full', 'full', 0, 'default', 'jpg')
+
+
+	# content_type refresh
+	def refresh_content_type(self):
+
+		# figure hierarchy
+		self.object_hierarchy(overwrite=True)
+
+
---
+++ ouroboros_assets/bag_classes/lgbt_detroit_ingest_workflow.py
@@ -0,0 +1,456 @@
+# Archivematica
+
+import uuid, json, os
+import bagit
+from lxml import etree
+import mimetypes
+import metsrw
+
+from WSUDOR_Manager import utilities, models, logging
+from inc import WSUDOR_bagger
+
+
+# define required `BagClass` class
+class BagClass(object):
+	
+		
+	# class is expecting a healthy amount of input from `ingestWorkspace` script, and object row
+	def __init__(self, object_row, ObjMeta, bag_root_dir, files_location, purge_bags):
+
+		# hardcoded
+		self.name = 'Archivematica_Hierarchical'  # human readable name, ideally matching filename, for this bag creating class
+		self.content_type = False  # not required, but easy place to set the WSUDOR_ContentType
+
+		# passed
+		self.object_row = object_row  # handle for object mysql row in 'ingest_workspace_object'
+		self.ObjMeta = ObjMeta  # ObjMeta class from ouroboros.models
+		self.bag_root_dir = bag_root_dir  # path for depositing formed bags
+		self.files_location = files_location  # location of files: they might be flat, nested, grouped, etc.
+		
+		# derived from object_row
+		self.MODS = object_row.MODS  # MODS as XML string		
+		self.struct_map = json.loads(object_row.struct_map)  # JSON representation of structMap section from METS file for this object
+		self.object_title = (object_row.object_title[:100] + '..') if len(object_row.object_title) > 100 else object_row.object_title
+		self.DMDID = object_row.DMDID  # object DMDID from METS, probabl identifier for file (but not required, might be in MODS)
+		self.collection_identifier = object_row.job.collection_identifier  # collection signifier, likely suffix to 'wayne:collection[THIS]'
+		self.object_type = object_row.object_type
+		
+		self.purge_bags = purge_bags
+
+		# MODS_handle (parsed with etree)
+		try:
+			MODS_tree = etree.fromstring(self.MODS)
+			ns = MODS_tree.nsmap
+			self.MODS_handle = MODS_root.xpath('//mods:mods', namespaces=ns)[0]
+		except:
+			logging.debug("could not parse MODS from DB string"		)
+
+		# future
+		self.objMeta_handle = None
+
+		# generate obj_dir
+		self.obj_dir = "/".join( [bag_root_dir, str(uuid.uuid4())] ) # UUID based hash directory for bag
+		if not os.path.exists(self.obj_dir):
+			# make root dir
+			os.mkdir(self.obj_dir)
+			# make data dir
+			os.mkdir("/".join([self.obj_dir,"datastreams"]))	
+
+		# attempt to grab intellectual type
+		if 'mets:div' in self.struct_map:
+			self.intellectual_type = self.struct_map['mets:div']['@TYPE']
+		elif 'ns0:div' in self.struct_map:
+			self.intellectual_type = self.struct_map['ns0:div']['@TYPE']
+		else:
+			self.intellectual_type = 'Item'
+		self.intellectual_type = self.intellectual_type.lower()
+
+		# DEBUG
+		logging.debug("#########################################")
+		logging.debug(self.intellectual_type)
+		logging.debug("#########################################")
+
+		# set collection pid
+		self.collection_pid = "wayne:collection%s" % self.collection_identifier
+
+
+	# method to create bag
+	def createBag(self):
+
+		if not self.object_row.aem_enriched:
+			# set identifier with filename
+			self.full_identifier = self.collection_identifier+self.object_title.replace(".","_")
+
+			# generate PID
+			self.pid = "wayne:%s" % (self.full_identifier)
+
+			# set collection pid to collection_identifier
+			if self.intellectual_type == 'collection':
+				logging.debug("setting collection pid special")
+				self.pid = self.collection_pid
+
+		else:			
+
+			self.pid = self.object_row.pid
+
+			# set collection pid to collection_identifier
+			if self.intellectual_type == 'collection':
+				logging.debug("setting collection pid special")
+				self.pid = self.collection_pid
+
+			self.full_identifier = self.pid.split("wayne:")[-1]
+
+		# write MODS
+		with open("%s/MODS.xml" % (self.obj_dir), "w") as fhand:
+			fhand.write(self.MODS)
+
+		# write PREMIS datastream
+		# if self.intellectual_type == 'item':
+		# 	with open("%s/PREMIS.xml" % (self.obj_dir), "w") as fhand:
+				
+		# 		# isntantiate PREMISClient
+		# 		pc = models.PREMISClient()
+		# 		for event in json.loads(self.object_row.premis_events):
+		# 			pc.add_event_xml(event)
+		# 		fhand.write(pc.as_string(pretty_print=False))
+
+		# if collection, write ingest METS to bag
+		if self.intellectual_type == 'collection':
+
+			logging.debug("writing ingest and enrichmente METS files")
+
+			# archivematica METS
+			if self.object_row.job.ingest_metadata:
+				with open("%s/archivematica_METS.xml" % (self.obj_dir), "w") as fhand:
+					fhand.write(self.object_row.job.ingest_metadata)
+
+			# enrichment METS (from ArchivesSpace)
+			if self.object_row.job.enrichment_metadata:
+				with open("%s/aspace_enrichment_METS.xml" % (self.obj_dir), "w") as fhand:
+					fhand.write(self.object_row.job.enrichment_metadata)
+
+		# instantiate object with quick variables
+		objMeta_primer = {
+			"id": self.pid,
+			"identifier": self.full_identifier,
+			"label": self.object_title,
+			"intellectual_type": self.intellectual_type			
+		}
+
+		# Instantiate ObjMeta object
+		self.objMeta_handle = self.ObjMeta(**objMeta_primer)
+		
+		# determine if physical or intellectual processing
+		if self.object_type in ['Item','Directory']:
+			result = self._createBag_physical()
+		elif self.object_type in ['Intellectual']:
+			result = self._createBag_intellectual()
+		else:
+			logging.debug("object_type %s not understood, cancelling" % self.object_type)
+			return False
+
+		# write known relationships
+		add_rels = [			
+			{
+				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/isDiscoverable",
+				"object": "info:fedora/True"
+			},
+			{
+				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/preferredContentModel",
+				"object": "info:fedora/CM:%s" % (self.content_type.split("_")[1])
+			},
+			{
+				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasSecurityPolicy",
+				"object": "info:fedora/wayne:WSUDORSecurity-permit-apia-unrestricted"
+			}
+		]
+		for rel in add_rels:
+			self.objMeta_handle.object_relationships.append(rel)
+
+		# if not collection object, make member of
+		if self.intellectual_type != 'collection':
+			self.objMeta_handle.object_relationships.append({
+				"predicate": "info:fedora/fedora-system:def/relations-external#isMemberOfCollection",
+				"object": "info:fedora/wayne:collection%s" % (self.collection_identifier)
+			})
+
+		# update id and identifier from pid
+		self.objMeta_handle.id = self.pid
+		self.objMeta_handle.identifier = self.pid.split(":")[-1]
+
+		# write to objMeta.json file
+		self.objMeta_handle.writeToFile("%s/objMeta.json" % (self.obj_dir))
+
+		# use WSUDOR bagger (NO MD5 CHECKSUMS)
+		bag = WSUDOR_bagger.make_bag(self.obj_dir, {
+			'Collection PID' : "wayne:collection"+self.collection_identifier,
+			'Object PID' : self.pid
+		})
+
+		return self.obj_dir
+
+	############################################################
+	# Archivematica / Physical Object
+	############################################################
+	def _createBag_physical(self):
+
+		# set namespaces
+		ns = {
+			'mets':'http://www.loc.gov/METS/',
+			'premis':'info:lc/xmlns/premis-v2',
+			'fits':'http://hul.harvard.edu/ois/xml/ns/fits/fits_output'
+		}		
+
+		# open mets		
+		if not self.object_row.metsrw_parsed:
+			logging.debug("##################### could not find parsed METS, parsing now")
+			temp_filename = '/tmp/Ouroboros/%s.xml' % uuid.uuid4()
+			with open(temp_filename, 'w') as fhand:
+				fhand.write(self.object_row.job.ingest_metadata.encode('utf-8'))
+			mets = metsrw.METSDocument.fromfile(temp_filename)
+			os.remove(temp_filename)
+		else:
+			mets = self.object_row.metsrw_parsed
+
+		# Build datastream dictionary
+		'''
+		Use label instead, as object_title might update?
+		'''
+		label = self.struct_map['ns0:div']['@LABEL']
+		fs = fsByLabel(mets, label)
+		# fs = fsByLabel(mets, self.object_title)
+		logging.debug(fs)
+
+		# determine parent
+		try:
+			parent_label = fs.parent.label.replace(".","_")
+			parent_pid = "wayne:%s%s" % (self.collection_identifier, parent_label)
+		except:
+			logging.debug("Parent not found, setting collection PID")
+			parent_pid = "wayne:collection%s" % (self.collection_identifier)
+
+		# write parent
+		self.objMeta_handle.object_relationships.append({
+			"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasPhysicalParent",
+			"object": "info:fedora/%s" % parent_pid
+		})
+
+		# open WSU enrichment METS
+		if self.object_row.aem_enriched:
+			enrichment_METS = etree.fromstring(self.object_row.job.enrichment_metadata.encode('utf-8'))
+			# get parent, if it's a directory, then set collection parent, else default to collection
+			# try:
+			if self.object_type == 'Directory':
+				logging.debug("Looking for %s to determine parent" % self.DMDID)
+				parent_pid = self.collection_pid		
+				logging.debug("Anticipating and setting hasParent pid: %s" % parent_pid)
+				self.objMeta_handle.object_relationships.append({
+					"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasParent",
+					"object": "info:fedora/%s" % parent_pid
+				})
+
+			else:		
+				logging.debug("Looking for %s to determine parent" % self.DMDID)
+				self_div = enrichment_METS.xpath('//mets:div[@DMDID="%s"]' % (self.DMDID), namespaces=ns)[0]
+				parent_div = self_div.getparent()
+				parent_DMDID = parent_div.attrib['DMDID']
+				parent_pid = 'wayne:%s%s' % (self.collection_identifier, parent_DMDID.split("aem_prefix_")[-1].replace(".","_"))		
+				logging.debug("Anticipating and setting hasParent pid: %s" % parent_pid)
+				self.objMeta_handle.object_relationships.append({
+					"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasParent",
+					"object": "info:fedora/%s" % parent_pid
+				})
+
+		# for "Directory" types
+		'''
+		Has much less information than files, but does include parent
+		'''
+		if self.object_type == 'Directory':
+			
+			# set content types
+			self.content_type = "WSUDOR_Container"
+			self.objMeta_handle.content_type = self.content_type
+
+			# set isRepresentedBy
+			self.objMeta_handle.isRepresentedBy = False
+
+		# for "Item" types
+		'''
+		With binary files, comes more information.  We can derive mimetype, and thus ContentType, from the fits metadata"
+		'''
+		if self.object_type == "Item":
+
+			# remove file suffix from PID
+			if not self.object_row.aem_enriched:			
+				self.pid = "_".join(self.pid.split("_")[:-1])
+
+			# determine mimetype
+
+			# APPROACH #1 : use Archivematica analysis of file
+			try:
+				logging.debug("Attempting to derive mimetype from Archivematica output")
+				# get amd section
+				amd = fs.amdsecs[0]
+
+				# get techMD as lxml element
+				for sub in amd.subsections:
+					if sub.id_string().startswith('techMD'):
+						techMD = sub.serialize()
+
+				# get identity section from fits tool, via premis event
+				fits_identity = techMD.xpath('//premis:objectCharacteristicsExtension/fits:fits/fits:identification/fits:identity',namespaces=ns)[0]
+
+				# get mimetype
+				derived_mime_type = fits_identity.attrib['mimetype']
+
+				# Double check it's not an iso file
+				# get file extension
+				file_ext = fs.path.split(".")[-1]				
+
+				# use mimetypes library
+				mimetypes.init()
+				if file_ext == "iso":
+					derived_mime_type = "application/octet-stream"
+				else:
+					derived_mime_type = fits_identity.attrib['mimetype']
+				print "derivedasdfasdf"
+				print derived_mime_type
+			
+			# APPROACH #2 : fall back on file extension			
+			except:
+				logging.debug("Could not determine mime type of file from Archivematica output, attempting file extension")
+
+				# get file extension
+				file_ext = fs.path.split(".")[-1]				
+
+				# use mimetypes library
+				mimetypes.init()
+				if file_ext == "iso":
+					derived_mime_type = "application/octet-stream"
+				else:
+					derived_mime_type = mimetypes.types_map[".%s" % file_ext]
+
+			# Set content type here
+			if derived_mime_type in utilities.mime_CM_hash:
+				self.content_type = utilities.mime_CM_hash[derived_mime_type]
+				self.objMeta_handle.content_type = self.content_type
+				print "STUDFSDF"
+				print self.content_type
+			else:
+				logging.debug("could not determine mimetype from fits, skipping")
+				return False
+
+			# write datastream
+			self._makeDatastream(fs)
+
+
+	############################################################
+	# Intellectual Object
+	############################################################
+	def _createBag_intellectual(self):
+
+		ns = {
+			'mets':'http://www.loc.gov/METS/'
+		}
+
+		# set content type
+		self.content_type = "WSUDOR_Container"
+		self.objMeta_handle.content_type = self.content_type
+
+		# set isRepresentedBy
+		self.objMeta_handle.isRepresentedBy = False
+
+		# if Intellectual object, and not collection, change to anticipated PID
+		if self.intellectual_type != "collection":
+			self.pid = 'wayne:%s%s' % (self.collection_identifier, self.DMDID.split("aem_prefix_")[-1])
+
+		# open WSU enrichment METS
+		enrichment_METS = etree.fromstring(self.object_row.job.enrichment_metadata.encode('utf-8'))
+
+		# get parent, else default to collection
+		try:
+			self_div = enrichment_METS.xpath('//mets:div[@DMDID="%s"]' % (self.DMDID), namespaces=ns)[0]
+			parent_div = self_div.getparent()
+			parent_DMDID = parent_div.attrib['DMDID']
+			parent_pid = 'wayne:%s%s' % (self.collection_identifier, parent_DMDID.split("aem_prefix_")[-1])		
+			logging.debug("Anticipating parent pid: %s" % parent_pid)
+
+			if self.intellectual_type == 'series':
+				logging.debug("series detected, pointing parent at collection object")
+				parent_pid = self.collection_pid
+
+			self.objMeta_handle.object_relationships.append({
+				"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasParent",
+				"object": "info:fedora/%s" % parent_pid
+			})
+
+		except:
+			logging.debug("Parent DMDID not found in enrichment METS")
+			if self.object_type == 'collection':
+				parent_pid = "wayne:collection%s" % (self.collection_identifier)
+				self.objMeta_handle.object_relationships.append({
+					"predicate": "http://digital.library.wayne.edu/fedora/objects/wayne:WSUDOR-Fedora-Relations/datastreams/RELATIONS/content/hasParent",
+					"object": "info:fedora/%s" % parent_pid
+				})	
+			else:
+				logging.debug("skipping parent PID"			)
+
+
+	# helper function to build datastream directory
+	def _makeDatastream(self, fs):
+
+		# Identify datastreams folder
+		datastreams_dir = self.obj_dir + "/datastreams"
+
+		filename = fs.path.split("/")[-1]
+		label = fs.label
+		'''
+		Need to resolve issue of order...
+		'''
+		order = 1 
+
+		# get extension, ds_id
+		mimetypes.init()
+		ds_id, ext = os.path.splitext(filename)
+		if ext == ".iso":
+			mt = "application/octet-stream"
+		else:
+			mt = mimetypes.types_map[ext]
+
+		# fix ds_id and label
+		ds_id = "DS%s" % ds_id
+		label = "DS%s" % label
+
+		# create datastream dictionary
+		ds_dict = {
+			"filename": filename,
+			"ds_id": ds_id,
+			"mimetype": mt,
+			"label": label,
+			"internal_relationships": {},
+			'order': order
+		}
+
+		self.objMeta_handle.datastreams.append(ds_dict)
+
+		# make symlinks to datastreams on disk
+		bag_location = datastreams_dir + "/" + filename
+
+		# determine remote_location by parsing filename
+		remote_location = self.files_location + "/" + fs.path
+		os.symlink(remote_location, bag_location)
+
+		# Set the representative image for the object
+		if int(order) == 1:
+			self.objMeta_handle.isRepresentedBy = ds_id
+
+
+# HELPER FUNCTIONS (TO MOVE)
+def fsByLabel(mets,label):
+	for fs in mets.all_files():
+		if fs.label == label:
+			return fs
+
+
+
+
